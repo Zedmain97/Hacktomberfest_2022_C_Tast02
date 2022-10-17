f5a03bb5ee9abd9b5a15505c27e7769aedf76032
ChamikaPF <40482275+ChamikaPF@users.noreply.github.com>
2022-10-17 19:36:55 +0530

    Update Hacktomberfest_2022_C_Tast02.c

--- Hacktomberfest_2022_C_Tast02.c
+++ Hacktomberfest_2022_C_Tast02.c
@@ -1,15 +1,15 @@
 int sum(int k);
 
 int main() {
-  inting result = sum(10)
-  printf("%d", result);
-  return 0;
+    int result = sum(10);
+    printf("%d", result);
+    return 0;
 }
 
 int sum(int k) {
-  if (k > 0) {
-    return k + sum(k - 1)
-  } else {
-    return;
-  }
+    if (k > 0) {
+        return k + sum(k - 1);
+    } else {
+        return 0;
+    }
 }
